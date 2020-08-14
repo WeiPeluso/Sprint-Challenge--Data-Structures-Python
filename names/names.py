41541486585c29044515806ac6c49dfcba610852
Wei Peluso <weipeluso@MacBook-Pro.local>
2020-08-14 12:33:40 -0400

    finished mvp and stretch

--- names/names.py
+++ names/names.py
@@ -18,7 +18,51 @@ duplicates = []  # Return the list of duplicates in this data structure
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-duplicates = list(set(names_1).intersection(names_2))
+# we can use binary search tree to solove this
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = Node(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = Node(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
+
+
+# first use name_1 to build a tree
+bst = Node(names_1[0])
+for name in names_1:
+    bst.insert(name)
+
+# second check if the names in names_2 is in the tree
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -28,3 +72,8 @@ print(f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+duplicatesStretch = list(set(names_1).intersection(names_2))
+
+end_time = time.time()
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime for stretch Goal is : {end_time - start_time} seconds")
